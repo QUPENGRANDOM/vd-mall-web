513e907eb42c6996abc3542bd173f6f9f2213da3
pengq <994004869@qq.com>
2020-05-28 23:06:40 +0800

    用户页

--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -8,35 +8,40 @@
       fit
       highlight-current-row
     >
-      <el-table-column align="center" label="序号" width="95">
+      <el-table-column align="center" label="序号">
         <template slot-scope="scope">
           {{ scope.$index + 1 }}
         </template>
       </el-table-column>
-      <el-table-column label="用户名称">
+      <el-table-column label="昵称" align="center">
         <template slot-scope="scope">
           {{ scope.row.nickname }}
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="登录名" align="center">
         <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
+          <span>{{ scope.row.username }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column label="地址" align="center">
         <template slot-scope="scope">
-          {{ scope.row.pageviews }}
+          {{ scope.row.address }}
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
+      <el-table-column label="所属角色" align="center">
+        <template slot-scope="scope">
+          {{ scope.row.roleNames | roleFilter }}
+        </template>
+      </el-table-column>
+      <el-table-column class-name="status-col" label="账号状态" align="center">
         <template slot-scope="scope">
           <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+      <el-table-column align="center" prop="created_at" label="注册时间">
         <template slot-scope="scope">
           <i class="el-icon-time" />
-          <span>{{ scope.row.display_time }}</span>
+          <span>{{ scope.row.createTime }}</span>
         </template>
       </el-table-column>
     </el-table>
@@ -61,11 +66,13 @@ export default {
   filters: {
     statusFilter(status) {
       const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
+        ENABLED: 'success',
+        DISABLED: 'danger'
       }
       return statusMap[status]
+    },
+    roleFilter(roles) {
+      return roles === null ? '' : roles.toString()
     }
   },
   data() {
